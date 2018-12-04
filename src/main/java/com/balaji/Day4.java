3f2e01746bb5f12e2e6a696acd6092f298009739
Balaji Damodaran <damodaran.balaji@gmail.com>
2018-12-04 21:18:24 +0100

    day 4.

--- src/main/java/com/balaji/Day4.java
+++ src/main/java/com/balaji/Day4.java
@@ -1,55 +1,69 @@
 package com.balaji;
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.IntStream;
 
 public class Day4 {
 
-  public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    String line;
-    String[] input = new String[1200];
-    int count = 0;
-    while (!(line = scanner.nextLine()).equals("")) {
-      input[count++] = line;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String line;
+        String[] input = new String[1200];
+        int count = 0;
+        while (!(line = scanner.nextLine()).equals("")) {
+            input[count++] = line;
+        }
+        Arrays.sort(input, 0, count);
+
+        int i = -1;
+        String id = null;
+        HashMap<String, Integer[]> att2 = new HashMap<>();
+        while (i < count - 1) {
+            if (input[++i].contains("Guard")) {
+                if (input[i + 1].contains("Guard")) continue;
+                id = input[i].split(" ")[3].substring(1);
+                att2.putIfAbsent(id, newArray());
+            }
+
+            do {
+                String sleep = input[++i];
+                String wakes = input[++i];
+
+                String sleepTime = sleep.substring(1, sleep.indexOf(']'));
+                String wakeUpTime = wakes.substring(1, wakes.indexOf(']'));
+
+                final String fid = id;
+                IntStream.range(minutes(sleepTime), minutes(wakeUpTime))
+                        .forEach(t -> att2.get(fid)[t]++);
+            } while (i < count - 1 && !input[i + 1].contains("Guard"));
+        }
+
+        long max = 0;
+        int maxIndex = 0;
+        String id2 = null;
+        for (Map.Entry<String, Integer[]> stringEntry : att2.entrySet()) {
+            //part 1
+            // long c = Arrays.stream(stringEntry.getValue()).filter(h -> h != 0).mapToInt(Integer::intValue).sum();
+            // part 2
+            long c = Arrays.stream(stringEntry.getValue()).max(Comparator.naturalOrder()).get();
+            if (max < c) {
+                max = c;
+                id2 = stringEntry.getKey();
+                List<Integer> list = Arrays.asList(stringEntry.getValue());
+                maxIndex = list.stream().max(Comparator.naturalOrder())
+                        .map(list::indexOf).orElse(-1);
+            }
+        }
+        System.out.println(Integer.parseInt(id2) * maxIndex);
     }
-    Arrays.sort(input, 0, count);
 
-    final String[][] data = new String[count][62];
-    for (String[] datum : data) {
-      Arrays.fill(datum, "");
+    private static Integer[] newArray() {
+        Integer[] integers = new Integer[60];
+        Arrays.fill(integers, 0);
+        return integers;
     }
 
-    int i = -1, idx = -1;
-    String id;
-    while (i < count - 1) {
-      if (input[++i].contains("Guard")) {
-        id = input[i].split(" ")[3];
-        data[++idx][0] = id;
-      }
-
-      do {
-        String sleep = input[++i];
-        String wakes = input[++i];
-
-        String sleepTime = sleep.substring(1, sleep.indexOf(']'));
-        String wakeUpTime = wakes.substring(1, wakes.indexOf(']'));
-
-        data[idx][1] = String.format("%s", sleepTime.split(" ")[0].substring(5));
-
-        final int v = idx;
-        IntStream.range(minutes(sleepTime), minutes(wakeUpTime))
-            .forEach(t -> data[v][t] = "#");
-      } while (i < count - 1 && !input[i + 1].contains("Guard"));
+    private static int minutes(String sleepTime) {
+        return Integer.parseInt(sleepTime.split(" ")[1].split(":")[1]);
     }
-
-    for (String[] datum : data) {
-      System.out.println(Arrays.toString(datum));
-    }
-  }
-
-  private static int minutes(String sleepTime) {
-    return Integer.parseInt(sleepTime.split(" ")[1].split(":")[1]);
-  }
 }
