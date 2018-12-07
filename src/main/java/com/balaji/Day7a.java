5e8fe25189f6c8f5d880de03a21f9e0f53dc8147
Balaji Damodaran <balaji@spotify.com>
2018-12-07 10:01:57 +0100

    part 1

--- src/main/java/com/balaji/Day7a.java
+++ src/main/java/com/balaji/Day7a.java
@@ -2,119 +2,16 @@ package com.balaji;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
-public class Day7a {
-  public static void main(String[] args) {
-    Search search = new Search();
-    search.insert('F','P');
-    search.insert('R','J');
-    search.insert('X','H');
-    search.insert('L','N');
-    search.insert('U','Z');
-    search.insert('B','C');
-    search.insert('S','C');
-    search.insert('N','Y');
-    search.insert('I','J');
-    search.insert('H','K');
-    search.insert('G','Z');
-    search.insert('Q','V');
-    search.insert('E','P');
-    search.insert('P','W');
-    search.insert('J','D');
-    search.insert('V','W');
-    search.insert('T','D');
-    search.insert('Z','A');
-    search.insert('K','A');
-    search.insert('Y','O');
-    search.insert('O','W');
-    search.insert('C','M');
-    search.insert('D','A');
-    search.insert('W','M');
-    search.insert('M','A');
-    search.insert('C','A');
-    search.insert('F','Z');
-    search.insert('I','A');
-    search.insert('W','A');
-    search.insert('T','C');
-    search.insert('S','K');
-    search.insert('B','J');
-    search.insert('O','A');
-    search.insert('Q','P');
-    search.insert('G','M');
-    search.insert('R','T');
-    search.insert('B','G');
-    search.insert('J','O');
-    search.insert('X','E');
-    search.insert('X','C');
-    search.insert('H','Y');
-    search.insert('Y','A');
-    search.insert('X','W');
-    search.insert('H','A');
-    search.insert('X','A');
-    search.insert('I','M');
-    search.insert('G','J');
-    search.insert('N','G');
-    search.insert('D','M');
-    search.insert('L','D');
-    search.insert('V','T');
-    search.insert('I','Y');
-    search.insert('S','J');
-    search.insert('K','Y');
-    search.insert('F','R');
-    search.insert('U','T');
-    search.insert('Z','M');
-    search.insert('T','Z');
-    search.insert('B','I');
-    search.insert('E','K');
-    search.insert('N','J');
-    search.insert('X','Q');
-    search.insert('F','Y');
-    search.insert('H','P');
-    search.insert('Z','D');
-    search.insert('V','O');
-    search.insert('E','C');
-    search.insert('V','C');
-    search.insert('P','A');
-    search.insert('B','N');
-    search.insert('S','W');
-    search.insert('P','D');
-    search.insert('L','W');
-    search.insert('D','W');
-    search.insert('K','C');
-    search.insert('L','M');
-    search.insert('R','O');
-    search.insert('F','L');
-    search.insert('R','H');
-    search.insert('K','O');
-    search.insert('T','W');
-    search.insert('R','K');
-    search.insert('C','W');
-    search.insert('N','T');
-    search.insert('R','P');
-    search.insert('E','M');
-    search.insert('G','T');
-    search.insert('U','K');
-    search.insert('Q','D');
-    search.insert('U','S');
-    search.insert('J','V');
-    search.insert('P','Y');
-    search.insert('X','Z');
-    search.insert('U','H');
-    search.insert('H','M');
-    search.insert('I','C');
-    search.insert('V','M');
-    search.insert('N','I');
-    search.insert('B','K');
-    search.insert('R','Q');
-    search.insert('O','C');
-
-    search.traverse();
-  }
-
+class Day7a {
   static class Search {
+
     Map<Character, Node> x = new HashMap<>();
 
     void insert(char a, char b) {
@@ -127,33 +24,31 @@ public class Day7a {
     }
 
     void traverse() {
+      final HashSet<Node> freeNodes = new HashSet<>();
       for (Node value : x.values()) {
-        traverse(value);
-        reset();
-        System.out.println();
+        if (!value.nextParent().isPresent()) {
+          freeNodes.add(value);
+        }
       }
+      final Node node = freeNodes.stream().sorted().findFirst().orElse(null);
+      final StringBuilder builder = new StringBuilder();
+
+      freeNodes.remove(node);
+      traverse(node, builder, freeNodes);
+      System.out.println(builder.toString());
     }
 
-    void reset() {
-      for (Node value : x.values()) {
-        value.setVisited(false);
-      }
-    }
-
-    void traverse(Node n) {
+    void traverse(Node n, StringBuilder builder, Set<Node> freeNodes) {
       if (n.isVisited()) {
         return;
       }
-
-      while (n.nextParent().isPresent()) {
-        n = n.nextParent().get();
-      }
-      n.setVisited(true);
-      System.out.print(n.name);
-
-      if (n.nextChild().isPresent()) {
-        traverse(n.nextChild().get());
-      }
+      n.setVisited();
+      builder.append(n.name);
+      freeNodes.addAll(n.availableFree());
+      freeNodes.stream().sorted().filter(q -> !q.isVisited()).findFirst().ifPresent(d -> {
+        freeNodes.remove(d);
+        traverse(d, builder, freeNodes);
+      });
     }
   }
 
@@ -168,8 +63,8 @@ public class Day7a {
       this.visited = false;
     }
 
-    void setVisited(boolean v) {
-      this.visited = v;
+    void setVisited() {
+      this.visited = true;
     }
 
     void addChild(Node child) {
@@ -181,12 +76,12 @@ public class Day7a {
       return visited;
     }
 
-    Optional<Node> nextChild() {
-      return children.stream().filter(n -> !n.isVisited()).sorted().findFirst();
-    }
-
     Optional<Node> nextParent() {
       return parents.stream().filter(n -> !n.isVisited()).sorted().findFirst();
+    }
+
+    Set<Node> availableFree() {
+      return children.stream().filter(n -> !n.nextParent().isPresent()).collect(Collectors.toSet());
     }
 
     @Override
