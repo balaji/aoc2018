963e3490e1d531fd7c2df0152c0bdf7812d6d2ce
Balaji Damodaran <balaji@spotify.com>
2018-12-07 07:24:48 +0100

    wip

--- src/main/java/com/balaji/Day7a.java
+++ src/main/java/com/balaji/Day7a.java
@@ -1,4 +1,55 @@
 package com.balaji;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 public class Day7a {
+  static class Node implements Comparable {
+    private boolean visited;
+    private Character name;
+    private List<Node> parents = new ArrayList<>();
+    private List<Node> children = new ArrayList<>();
+
+    Node(char name) {
+      this.name = name;
+    }
+
+    public void setVisited(boolean visited) {
+      this.visited = visited;
+    }
+
+    public void addParent(Node parent) {
+      this.parents.add(parent);
+    }
+
+    public void addChild(Node parent) {
+      this.children.add(parent);
+    }
+
+    public boolean isVisited() {
+      return visited;
+    }
+
+    public Optional<Node> nextChild() {
+      return children.stream().sorted().findFirst();
+    }
+
+    public Optional<Node> nextParent() {
+      return parents.stream().sorted().findFirst();
+    }
+
+    public boolean hasActiveParents() {
+      return parents.stream().anyMatch(node -> !node.isVisited());
+    }
+
+    public boolean hasActiveChildren() {
+      return children.stream().anyMatch(node -> !node.isVisited());
+    }
+
+    @Override
+    public int compareTo(Object o) {
+      return ((Node) o).name.compareTo(this.name);
+    }
+  }
 }
